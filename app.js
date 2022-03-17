2e9abe04d9facd879edbfb6a25d9716504e23888
habeeb-ng <77938563+habeeb-ng@users.noreply.github.com>
2022-03-17 16:45:29 -0500

    Successful expense tracker

--- app.js
+++ app.js
@@ -11,35 +11,51 @@ function Expenses(name, date, amount) {
     this.date = date;
     this.amount = amount;
 }
-
+totalExpense.textContent = 0;
 let allExpense = [];
+let existExpense = false;
 
 // add datas to arrays
-function newExpObj(){
-    const create = new Expenses(expenseName.value, expenseDate.value, expenseAmount.valueAsNumber);
+function newExpObj() {
+    //check if the array exist in the dom expense list
+    for (each of allExpense) {
+        if (expenseName.value === each.name) {
+            alert(expenseName.value + " is already an expense int the list, enter a different expense name");
+            existExpense = true;
+            return false;
+        }
+    }
+        const create = new Expenses(expenseName.value, expenseDate.value, expenseAmount.valueAsNumber);
     allExpense.push(create);
+    for(let i =0; i < allExpense.length; i++){
+    if(isNaN(allExpense[i].amount)){
+        allExpense[i].amount = 0;
+    }
 }
-
-expenseInfo.onsubmit = function(eventObj){
-    eventObj.preventDefault();
     
-    newExpObj();
-    // calcAmount();
-    createExpense();
-    expenseName.value= "";
-    expenseDate.value = "";
-    expenseAmount.value = "";
+    // }
+    existExpense = false;
+    return true;
 }
+// calculate the amounts
+    function calcAmount(){
+        let sum = 0;
+        for (let i = 0; i < allExpense.length; i++){
+            sum += allExpense[i].amount;
+        }
+    
+        totalExpense.textContent = sum;
+    }
+
 //create td
 function createTd (){
-    // let newExp = `<td>${exName}</td><td>${exDate}</td><td>${exAmount}</td><td onclick="del(this)" style="cursor:pointer" > delete</td>`;
-    // return newExp;
     return document.createElement('td');
 }
 //create expense table row
 function createExpense() {
     
     const newExpense = document.createElement("tr");
+    newExpense.setAttribute('id', expenseName.value);
     const exName = createTd();
     exName.append(expenseName.value)
     const exDate = createTd();
@@ -60,80 +76,39 @@ function createExpense() {
     newExpense.append(exAmount);
     newExpense.append(del);
 
-    // calculate the amounts
-    function calcAmount(){
-        let sum = 0;
-        for (let i = 0; i < allExpense.length; i++){
-            sum += allExpense[i].amount;
-        }
     
-        totalExpense.textContent = sum;
-    }
-    calcAmount()
+    calcAmount();
 
 
     expenseDatas.appendChild(newExpense);
 
     //delete feature
     del.addEventListener('click', (eventObj) => {
+        let deleName = eventObj.target.parentElement.children[0].innerHTML;
+        let delNum;
+        console.log(deleName);
+        for (let i = 0; i < allExpense.length; i++) {
+            if (allExpense[i].name === deleName) {
+                delNum = allExpense[i].amount;
+                allExpense.splice(i, 1);
+                calcAmount();
+
+            }
+        }
         eventObj.target.parentElement.remove();
-        // for (let each of allExpense) {
-        //     if (each.name === x) {
-                
-        //     }
-        // }
     } )
 }
-
-
-
-
-
-//the delete feature
-// function takeOut(){
-//     this.parentElement.remove();
-//     let sum = 0;
+//event listeb=ner for when a new expense is entered
+// main function.
+expenseInfo.onsubmit = function(eventObj){
+    eventObj.preventDefault();
     
-    // for(let i = 0; i < allExpense.length; i++){
-    //     if (delEelement.parentElement.remove() === allExpense[i]){
-    //         allExpense.splice(i, 1);
-    //     }
-    // }
-// }
-
-
-
-
-
-
-
-
-
-//  tried and error
-// for (let i = 0; i < allExpense.length; i++) {
-    //     const newExpense = document.createElement("tr");
-    //     const exName = createTd();
-    //     exName.append(allExpense[i].name)
-    //     const exDate = createTd();
-    //     exDate.append(allExpense[i].date)
-    //     const exAmount = createTd();
-    //     exAmount.append(allExpense[i].amount)
-
-    //     const del = createTd();
-    //     del.innerText = 'Del';
-    //     del.style.cursor = 'pointer'
-        
-        
-
-        
-    //     newExpense.append(exName);
-    //     newExpense.append(exDate);
-    //     newExpense.append(exAmount);
-    //     newExpense.append(del);
-
-
-    //     expenseDatas.appendChild(newExpense);
-    //     del.addEventListener('click', takeOut )
-        
-    // }
-
+    newExpObj();
+    if (!existExpense) {
+        createExpense();
+    }
+    
+    expenseName.value= "";
+    expenseDate.value = "";
+    expenseAmount.value = "";
+}
